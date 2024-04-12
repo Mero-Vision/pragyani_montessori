29af97d24bccb9aa8601cc5c85466f6554ee1703
Hancie123 <hanciewanemphago@gmail.com>
2024-04-12 17:54:44 +0545

    design updated

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,19 +1,26 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>Pragyani Montessori</title>
-        <meta name="author" content="Vecuro">
-        <meta name="description" content="Kiddino - Children School & Kindergarten HTML Template">
-        <meta name="keywords" content="Kiddino - Children School & Kindergarten HTML Template">
-        <meta name="robots" content="INDEX,FOLLOW">
+        <title>Pragyani Montessori & Childcare Center - New Naikap, Kathmandu</title>
         <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
-        <link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon">
-        <link rel="icon" href="assets/img/favicon.ico" type="image/x-icon">
+        <meta name="author" content="Hancie Phago">
+        <link rel="icon" href="{{url('assets/img/logo.ico')}}" type="image/x-icon">
+
+
+
+        <x-meta title="Pragyani Montessori & Childcare Center - New Naikap, Kathmandu"
+            description="Discover a nurturing learning environment for your child at Pragyani 
+            Montessori & Childcare Center, conveniently located on Pragati Sangam Marg in New Naikap,
+             Chandragiri 14, Kathmandu. "
+            image="{{url('assets/img/logo.jpg')}}" />
+
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@400;500;600;700&family=Jost:wght@400;500&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="{{url('assets/css/app.min.css')}}">
-        <link rel="stylesheet" href="{{url('assets/css/fontawesome.min.css')}}">
-        <link rel="stylesheet" href="{{url('assets/css/style.css')}}">
+        <link
+            href="https://fonts.googleapis.com/css2?family=Fredoka:wght@400;500;600;700&family=Jost:wght@400;500&display=swap"
+            rel="stylesheet">
+        <link rel="stylesheet" href="{{ url('assets/css/app.min.css') }}">
+        <link rel="stylesheet" href="{{ url('assets/css/fontawesome.min.css') }}">
+        <link rel="stylesheet" href="{{ url('assets/css/style.css') }}">
         <meta name="google-site-verification" content="OOmvogs6GdjgRqoOMYlMHt3tqSpMZGp-CZx2qXFdAI4" />
     </head>

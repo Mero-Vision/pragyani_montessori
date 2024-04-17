61b4729deea4b442e55af69d5a3f0ce8aa9a5ed4
Hancie123 <hanciewanemphago@gmail.com>
2024-04-18 00:00:09 +0545

    contact us added

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,7 +1,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>Pragyani Montessori & Childcare Center - New Naikap, Kathmandu</title>
+        <title>Pragyani Montessori & Childcare (PMC) - New Naikap, Kathmandu</title>
         <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
         <meta name="author" content="Hancie Phago">
         <link rel="icon" href="{{url('assets/img/logo.ico')}}" type="image/x-icon">
